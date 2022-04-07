b841e69d46e68bd34f2788b6e8324631a1b55dac
Hava Feder <94397782+havafeder@users.noreply.github.com>
2022-04-07 12:51:32 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 from flask import Flask, request, redirect, render_template
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
@@ -75,3 +75,60 @@ def edit_user(user_id):
     db.session.commit()
 
     return redirect("/")
+
+
+@app.route('/users/<int:user_id>/add_post')
+def post_form(user_id):
+    """go to add_post form"""
+    user = User.query.get_or_404(user_id)
+    first_name = user.first_name
+
+    return render_template('add_post.html', user=user, first_name=first_name)
+
+@app.route('/users/<int:user_id>/add_post', methods=["POST"])
+def add_post(user_id):
+    """Form submission for creating new post"""
+
+    user = User.query.get_or_404(user_id)
+    new_post = Post(title=request.form['title'],
+                    content=request.form['content'], user_id=user_id)
+    db.session.add(new_post)
+    db.session.commit()
+    return redirect(f"/{user_id}")
+
+@app.route('/posts/<int:post_id>')
+def show_posts(post_id):
+    """Show info on specific post"""
+
+    post = Post.query.get_or_404(post_id)
+    return render_template("show_posts.html", post=post)
+
+@app.route('/posts/<int:post_id>/edit', methods=["GET"])
+def edit_post(post_id):
+    """Show form to edit post"""
+
+    post = Post.query.get_or_404(post_id)
+    return render_template("edit_post.html", post=post)
+
+@app.route('/posts/<int:post_id>/edit', methods=["POST"])
+def update_post(post_id):
+    """Handle form submission for updating post"""
+
+    post = Post.query.get_or_404(post_id)
+    post.title = request.form['title']
+    post.content = request.form['content']
+
+    db.session.add(post)
+    db.session.commit()
+
+    return redirect(f"/{post.user_id}")
+
+@app.route('/posts/<int:post_id>/delete', methods=["POST"])
+def delete_post(post_id):
+    """Handle form submission for deleting a post"""
+
+    post = Post.query.get_or_404(post_id)
+
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(f"/{post.user_id}")
